688a84574e1bdd269b421647830b1e60fb46b2d5
William <23605383@sun.ac.za>
2021-03-08 00:45:00 +0200

    Setup server

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-import express from "express";
+const express = require('express');
 
+require('dotenv').config();
 
 app= express();
+
+app.listen(process.env.PORT || 3000,() =>{
+    console.log("Server opened on port "+process.env.PORT);
+});
+
+app.get('/',(req,res)=>{
+    res.send("Hello world from node");
+})
